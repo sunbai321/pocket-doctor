8197ebd57635cb3d12087e5a335d065f1efd0864
孙柏 <923345641@qq.com>
2017-12-26 10:21:33 +0800

    Delete zhong.js

--- js/zhong.js
+++
@@ -1,8 +0,0 @@
-var userID=localStorage.getItem('userID');
-if(userID){
-   $('.shezhi').attr('href','shezhi.html');
-   console.log('aaa');
- }
- else{
-  $('.shezhi').attr('href','denglu.html');
- }
