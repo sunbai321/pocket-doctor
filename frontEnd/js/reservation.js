df3a47a9f054455ded3835f1604a74b992df5c45
2015015413suyuanyuan <743688437@qq.com>
2018-01-12 10:29:17 +0800

    添加了提交 预约挂号表 的后台代码，优化了弹出框的页面

--- frontEnd/js/reservation.js
+++ frontEnd/js/reservation.js
@@ -1,34 +1,76 @@
 var docid = location.hash.slice(1);
+var reservation_userid=4;
+var reservation_hosid;
+var reservation_offid;
+var reservation_docid;
+var reservation_patid;
+var amount=20;
 var reservation_date;
+
+//完成页面渲染
 $.ajax({
 	type:'post',
 	url:'http://192.168.163.144:8080/reservation_list',
 	dataType:'JSON',
 	data:{doctorID:docid},
 	success:function(data){
+		reservation_hosid=data[0];
+		reservation_offid=data[1];
+		reservation_docid=data[2];
+		console.log('reservation_hosid',reservation_docid);
 		$('.yiyuan1').html(data[3]);
 		$('.name').html(data[5]+data[3]+data[4]);
 		console.log(data);
 	}
 });
 
-$(".genggai").click(function(){
-	$.ajax({
+//更改就诊人弹框渲染信息
+$.ajax({
 		type:"post",
 		url:'http://192.168.163.144:8080/patient_get',
 		dataType:"JSON",
 		data:{patientID:1},
 		success:function(data){
-			$('.sfz').html('身份证：'+data[0].IDcard);
-			$('.sjh').html('手机号：'+data[0].patientTel);
-			console.log(data);
-
+			//
+			for(var i=0;i<data.length;i++){
+				$('.btnbtn').html($('.btnbtn').html()+`
+					<button class='special_btn'>`+data[i].patientName+`</button>
+				`);
+				//console.log(data);
+			}
+			var btn_spe=$('.special_btn');
+			for(var k=0;k<btn_spe.length;k++){
+				(function(k){
+					$(btn_spe[k]).click(function(){	
+						reservation_patid=data[k].patientID;
+						$(".patient_name").html(data[k].patientName);
+						$('.sfz').html(data[k].IDcard);
+						$(".sjh").html(data[k].patientTel);
+						$('.special2').css('display','none');
+					})
+				})(k)
+			}
 		}
-	});
+});
+
+//点击更改，更改就诊人弹窗出现
+$(".genggai").click(function(){
+	$('.special2').css('display','block');
 });
 
 
+//点击取消，更改就诊人弹框消失
+$('.quxiao2').click(function(){
+	$('.special2').css('display','none');
+});
 
+//选择就诊时间弹框显示
+$(".time1").click(function(){
+	$(".special").css('display','block');
+});
+
+
+//完成就诊时间的获取
 $.ajax({
 	type:'post',
 	url:'http://192.168.163.144:8080/reservation_date',
@@ -46,7 +88,7 @@ $.ajax({
 		dateRadio[2].value=data[0].date3;
 		dateRadio[3].value=data[0].date4;
 
-		console.log(data);
+		//console.log(data);
 
 		$(".srue").click(function(){
 			for(var i=0;i<dateRadio.length;i++){
@@ -54,8 +96,9 @@ $.ajax({
 					if(dateRadio[i].checked){
 						$(".srue").attr('disabled',false);
 						//存储预约日期
-						localStorage.setItem('yiyueDate'+i,$(dateRadio[i]).val());
-						console.log($(dateRadio[i]).val());
+						reservation_date=$(dateRadio[i]).val();
+						console.log(reservation_date);
+						$(".time1").html($(dateRadio[i]).val());
 						$(".special").css('display','none');
 					}
 					else{
@@ -89,11 +132,10 @@ $.ajax({
 });
 
 
-$(".time1").click(function(){
-	$(".special").css('display','block');
+//显示添加就诊人弹框
+$('.tianjia').click(function(){
+	$('.special1').css('display','block');
 });
-
-
 
 
 //验证就诊人姓名
@@ -104,8 +146,8 @@ $('.pname').blur(function(){
 		$('.pname').val('');
 		$('.pname').attr('placeholder','格式不对，请重新输入');
 	}
-	
 });
+
 //验证就诊人身份证号
 $(".idcard").blur(function(){
 	var rge2=/[a-z]{2,}/gi;
@@ -129,7 +171,6 @@ $(".age").blur(function(){
 	if(rge4.test(val3) || val3.length>3 || val3.length<1){
 		$(this).val('');
 		$(this).attr('placeholder','年龄格式错误，请重新输入');
-
 	}
 });
 
@@ -144,13 +185,9 @@ $('.tel').blur(function(){
 	    }else if(telephone.length <11){
 	      $(this).val('');
 	      $(this).attr('placeholder','长度不够11位，请重新填写');
-
 	    }
 });
 
-$('.tianjia').click(function(){
-	$('.special1').css('display','block');
-});
 
 //添加就诊人信息，发ajax
 
@@ -162,7 +199,58 @@ $(".tian").click(function(){
 		dataType:"JSON",
 		data:{userid:1,name:$('.pname').val(),card:$('.idcard').val(),page:$('.age').val(),ptel:$('.tel').val()},
 		success:function(data){
-			console.log(data);
 		}
 	});
+	$('.special1').css('display','none');
+	$('.pname').val('');
+	$('.idcard').val('');
+	$('.age').val('');
+	$('.tel').val('');
 });
+
+
+//点击取消，添加就诊人弹框消失
+$('.quxiao1').click(function(){
+	$('.special1').css('display','none');
+});
+
+//在更改就诊人弹框中选择添加就诊人
+$(".btn1").click(function(){
+	$('.special1').css('display','block');
+	$('.special2').css('display','none');
+	$(".tian").click(function(){
+	console.log('发送成功,在更改');
+	$.ajax({
+		type:"post",
+		url:'http://192.168.163.144:8080/add_patient',
+		dataType:"JSON",
+		data:{userid:2,name:$('.pname').val(),card:$('.idcard').val(),page:$('.age').val(),ptel:$('.tel').val()},
+		success:function(data){	
+		}
+	});
+	$('.special1').css('display','none');
+	$('.pname').val('');
+	$('.idcard').val('');
+	$('.age').val('');
+	$('.tel').val('');
+});
+});
+
+//提交预约信息到后台，成功后实现页面跳转；
+$('.tijiao').click(function(){
+	console.log('dianjile ');
+	var checkbox2=$('.checkbox1');
+	if(checkbox2[0].checked){
+		$('.tijiao').css('background-color','pink');
+		$.ajax({
+			type:"post",
+			url:'http://192.168.163.144:8080/add_reservation',
+			dataType:'json',
+			data:{Ruserid:reservation_userid,Rhospital:reservation_hosid,Rdoctor:reservation_docid,Rpatient:reservation_patid,Ramount:amount,date:reservation_date},
+			success:function(data){
+
+			}
+
+		});//ajax结束
+	}
+})
