69b43ba7f34aa889b3c70082d9077764f84211f9
2015015413suyuanyuan <743688437@qq.com>
2018-01-13 17:44:23 +0800

    将所有网页归并到一个文件夹中，所有的网页模板已经都完成，注册登录功能已经实现

--- frontEnd/js/denglu.js
+++ frontEnd/js/denglu.js
@@ -10,23 +10,27 @@
 
   $(".load").click(function(){
   	$.ajax({
-      url:"http://datainfo.duapp.com/shopdata/userinfo.php",
-      data:{status:"login",userID:$(".name").val(),password:$('.password').val()},
+      url:"http://192.168.163.144:8080/validate_user",
+      dataType:'json',
+      type:'post',
+      data:{userTel:$(".name").val(),userPWD:$('.password').val()},
       success:function(data){
+        console.log(data);
+        //0 用户名不存在 1  密码不正确 2  密码用户名匹配
         if(data == 0){
           $(".name").val('');
-           $('.password').val('');
+          $('.password').val('');
           $(".name").attr('placeholder','用户名不存在');
         }
-        else if(data==2){
+        else if(data==1){
             $('.password').val('');
             $(".password").attr('placeholder','用户密码不符');
         }
         else{
             localStorage.setItem('username',$(".name").val());
             console.log(localStorage.getItem('username'));
-           window.location.href='zhuye.html';
-        }
+           window.location.href='index.html';
+       }
       }
     });
   });
