66f2f0ba38a0d0ea17b9629e7418ec3d30a9cfee
王晓晗 <814719883@qq.com>
2018-05-02 16:37:26 +0800

    设置页面

---
+++ frontEnd/js/patient.js
@@ -0,0 +1,8 @@
+
+$(".back").click(function(){
+	location.href="me.html";
+});
+$(".and1").click(function(){
+	location.href="add.html";
+});
+
---
+++ frontEnd/js/setup.js
@@ -0,0 +1,3 @@
+$(".back").click(function(){
+	location.href="me.html";
+})
